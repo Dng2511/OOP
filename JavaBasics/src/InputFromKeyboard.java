ee77d388c4dc1024ae80e1a79e04705787b58975
Dng <phamdong9854@gmail.com>
2024-09-29 11:23:14 +0700

    upload oop homework

--- JavaBasics/src/InputFromKeyboard.java
+++ JavaBasics/src/InputFromKeyboard.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class InputFromKeyboard {
     public static void main(String args[]){
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("What's your name?");
+        System.out.println("What's your name?");
         String name = keyboard.nextLine();
-        System.out.print("How old are you?");
+        System.out.println("How old are you?");
         int age = keyboard.nextInt();
-        System.out.print("How tall are you (m)?");
+        System.out.println("How tall are you (m)?");
         double height = keyboard.nextDouble();
         System.out.println("Mrs/Ms. " + name + ", " + age + " years old. Your height is " + height + ".");
     }
